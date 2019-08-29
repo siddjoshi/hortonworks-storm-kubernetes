f49a4a3ba760e5753437b468d42b064b3eb98edc
Siddharth <siddharth.joshi@bold.com>
2019-08-29 15:39:40 +0530

    changes to entrypoint

--- Storm/Docker/docker-entrypoint.sh
+++ Storm/Docker/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 # Allow the container to be started with `--user`
-if [ "$1" = 'storm' -a "$(id -u)" = '0' ]; then
-    chown -R storm:storm "$STORM_CONF_DIR" "$STORM_DATA_DIR" "$STORM_LOG_DIR"
-    exec gosu storm "$0" "$@"
-fi
+#if [ "$1" = 'storm' -a "$(id -u)" = '0' ]; then
+#    chown -R storm:storm "$STORM_CONF_DIR" "$STORM_DATA_DIR" "$STORM_LOG_DIR"
+#    exec gosu storm "$0" "$@"
+#fi
 # Generate the config only if it doesn't exist
 CONFIG="$STORM_CONF_DIR/storm.yaml"
 if [ ! -f "$CONFIG" ]; then
