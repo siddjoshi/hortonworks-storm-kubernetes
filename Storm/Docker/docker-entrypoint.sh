1b9d7f2df0b71c1a17b83e571a3d4089f46ca614
Siddharth <siddharth.joshi@bold.com>
2019-11-16 15:40:52 +0530

    added support for QA environment through DEPLOYMENT environment variable

--- Storm/Docker/docker-entrypoint.sh
+++ Storm/Docker/docker-entrypoint.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 set -e
 # Allow the container to be started with `--user`
-#if [ "$1" = 'storm' -a "$(id -u)" = '0' ]; then
-#    chown -R storm:storm "$STORM_CONF_DIR" "$STORM_DATA_DIR" "$STORM_LOG_DIR"
-#    exec gosu storm "$0" "$@"
-#fi
+if [ "$1" = 'storm' -a "$(id -u)" = '0' ]; then
+    chown -R storm:storm "$STORM_CONF_DIR" "$STORM_DATA_DIR" "$STORM_LOG_DIR"
+    exec gosu storm "$0" "$@"
+fi
 # Generate the config only if it doesn't exist
 CONFIG="$STORM_CONF_DIR/storm.yaml"
-if [ ! -f "$CONFIG" ]; then
+SETUP = "$DEPLOYMENT"
+if [ ! -f "$CONFIG" ]; 
+then
+if [ "$SETUP" = "QA" ];
+then
+
+    cat << EOF > "$CONFIG"
+storm.zookeeper.servers:
+  - "zk-0.zk-hs.default.svc.cluster.local"
+nimbus.seeds: ["nimbus-0.nimbus.default.svc.cluster.local"]
+storm.log.dir: "$STORM_LOG_DIR"
+storm.local.dir: "$STORM_DATA_DIR"
+EOF
+else
+
     cat << EOF > "$CONFIG"
 storm.zookeeper.servers:
   - "zk-0.zk-hs.default.svc.cluster.local"
@@ -18,6 +32,6 @@ storm.log.dir: "$STORM_LOG_DIR"
 storm.local.dir: "$STORM_DATA_DIR"
 EOF
 fi
+fi
 
 exec "$@"
-
